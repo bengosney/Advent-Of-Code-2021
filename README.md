b8d515d72bee8ff7bba5aa728cc858bc747a6ae0
Ben Gosney <bengosney@googlemail.com>
2021-11-29 17:26:19 +0000

    Tidy and add some tests for the utils

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Advent-Of-Code-2021
 
-Daily coding challenges for the month of December
+Solutions for daily coding challenges for the month of December
 
 [Take Part](https://adventofcode.com/2021)
--- src/utils.py
+++ src/utils.py
@@ -4,4 +4,19 @@ import os
 
 def getInput(day: int) -> str:
     with open(os.path.join(os.path.dirname(__file__), "..", "inputs", f"day-{day:02}.txt")) as f:
-        return f.read().strip()
+        return f.read().strip().strip("\n\r")
+
+
+def getInputLines(day: int) -> list[str]:
+    return getInput(day).split("\n")
+
+
+# --- tests
+
+
+def test_getInput():
+    assert getInput(0) == """test1\ntest2"""
+
+
+def test_getInputLines():
+    assert getInputLines(0) == ["test1", "test2"]
