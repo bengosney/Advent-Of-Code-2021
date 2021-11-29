c4df7663e7cc51d4f248b84c28c401a5162ba467
Ben Gosney <bengosney@googlemail.com>
2021-11-29 13:54:54 +0000

    Better layout

---
+++ src/utils.py
@@ -0,0 +1,7 @@
+# Standard Library
+import os
+
+
+def getInput(day: int) -> str:
+    with open(os.path.join(os.path.dirname(__file__), "..", "inputs", f"day-{day:02}.txt")) as f:
+        return f.read().strip()
