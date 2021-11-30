9d4083623bb1e2300e683eb26aad58bb5e2f5e9f
Ben Gosney <bengosney@googlemail.com>
2021-11-30 08:35:27 +0000

    Pointless work because it's the day before...

--- makefile
+++ makefile
@@ -4,6 +4,8 @@
 HOOKS=$(.git/hooks/pre-commit)
 PGCONTAINER:=stl-postgres
 
+DAYS=$(wildcard src/day_*.py)
+
 help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
@@ -34,3 +36,6 @@ clean:
 	@echo "Stopping any excisting postgres containers"
 	@docker stop $(PGCONTAINER) || true
 	@docker rm $(PGCONTAINER) || true
+
+test: $(DAYS)
+	pytest $(DAYS)
--- src/utils.py
+++ src/utils.py
@@ -2,21 +2,21 @@
 import os
 
 
-def getInput(day: int) -> str:
+def read_input(day: int) -> str:
     with open(os.path.join(os.path.dirname(__file__), "..", "inputs", f"day-{day:02}.txt")) as f:
         return f.read().strip().strip("\n\r")
 
 
-def getInputLines(day: int) -> list[str]:
-    return getInput(day).split("\n")
+def read_input_lines(day: int) -> list[str]:
+    return read_input(day).split("\n")
 
 
 # --- tests
 
 
-def test_getInput():
-    assert getInput(0) == """test1\ntest2"""
+def test_read_input():
+    assert read_input(0) == """test1\ntest2"""
 
 
-def test_getInputLines():
-    assert getInputLines(0) == ["test1", "test2"]
+def test_read_input_lines():
+    assert read_input_lines(0) == ["test1", "test2"]
