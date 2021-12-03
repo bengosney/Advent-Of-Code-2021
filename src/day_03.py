b655266329c0a4cde375ceb2f41fc5b085c6233a
Ben Gosney <bengosney@googlemail.com>
2021-12-03 08:37:19 +0000

    Day 3 part 2

--- src/day_03.py
+++ src/day_03.py
@@ -33,13 +33,58 @@ def epsilon(input: list[str]) -> int:
     return int(result, 2)
 
 
+def mostCommon(input: list[str], position: int) -> str:
+    bits = ["1"]
+    for row in input:
+        bits.append(row[position])
+
+    bits.append("0")
+
+    return Counter(bits).most_common()[0][0]
+
+
+def leastCommon(input: list[str], position: int) -> str:
+    bits = ["1"]
+    for row in input:
+        bits.append(row[position])
+
+    bits.append("0")
+
+    return Counter(bits).most_common()[-1][0]
+
+
+def o2(input: list[str], p=0) -> int:
+    left: list[str] = []
+
+    c = mostCommon(input, p)
+    left = [row for row in input if row[p] == c]
+
+    if len(left) == 1:
+        return int(left[0], 2)
+    else:
+        return o2(left, p + 1)
+
+
+def co2(input: list[str], p=0) -> int:
+    left: list[str] = []
+
+    c = leastCommon(input, p)
+    left = [row for row in input if row[p] == c]
+
+    if len(left) == 1:
+        return int(left[0], 2)
+    else:
+        return co2(left, p + 1)
+
+
 def part_1(input: str) -> int:
     lines = input.splitlines()
     return gamma(lines) * epsilon(lines)
 
 
 def part_2(input: str) -> int:
-    pass
+    lines = input.splitlines()
+    return o2(lines) * co2(lines)
 
 
 # -- Tests
@@ -73,8 +118,16 @@ def test_epsilon():
     assert epsilon(get_example_input().splitlines()) == 9
 
 
+def test_o2():
+    assert o2(get_example_input().splitlines()) == 23
+
+
+def test_co2():
+    assert co2(get_example_input().splitlines()) == 10
+
+
 def test_part_2():
-    assert part_2(get_example_input()) is not None
+    assert part_2(get_example_input()) == 230
 
 
 def test_part_1_real():
@@ -82,9 +135,10 @@ def test_part_1_real():
     assert part_1(input) == 3009600
 
 
-# def test_part_2_real():
-#     input = read_input(__file__)
-#     assert part_2(input) == None
+def test_part_2_real():
+    input = read_input(__file__)
+    assert part_2(input) == 6940518
+
 
 # -- Main
 
