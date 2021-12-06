7fb66242167bc6de46be744b224fd63d373d4cc8
Ben Gosney <bengosney@googlemail.com>
2021-12-06 08:21:50 +0000

    principal was sound, implementation was flawed

--- src/day_06.py
+++ src/day_06.py
@@ -4,18 +4,15 @@ from collections import defaultdict
 # First Party
 from utils import read_input
 
-# Third Party
-from icecream import ic
 
-
-def process_fish(fish: dict[int, int]) -> dict[int, int]:
+def process_fish(fish: dict[int, int], day: int) -> dict[int, int]:
     newFish = defaultdict(lambda: 0)
 
-    for i in range(8):
+    for i in range(9):
         newFish[i - 1] = fish[i]
 
-    newFish[8] = fish[-1]
-    newFish[6] = fish[-1]
+    newFish[8] = fish[0]
+    newFish[6] += fish[0]
     del newFish[-1]
 
     return newFish
@@ -28,22 +25,23 @@ def part_1(input: str) -> int:
     for f in fishInput:
         fish[f] += 1
 
-    for _ in range(18):
-        # ic(list(fish.values()))
-        for i in range(8):
-            ic(i, i - 1)
-            fish[i - 1] = fish[i]
+    for d in range(80):
+        fish = process_fish(fish, d + 1)
 
-        fish[8] = fish[-1]
-        fish[6] = fish[-1]
-        del fish[-1]
-        break
-
-    return len(fish)
+    return sum(list(fish.values()))
 
 
 def part_2(input: str) -> int:
-    pass
+    fishInput = map(int, input.split(","))
+    fish = defaultdict(lambda: 0)
+
+    for f in fishInput:
+        fish[f] += 1
+
+    for d in range(256):
+        fish = process_fish(fish, d + 1)
+
+    return sum(list(fish.values()))
 
 
 # -- Tests
@@ -58,19 +56,19 @@ def test_part_1():
     assert part_1(input) == 5934
 
 
-# def test_part_2():
-#     input = get_example_input()
-#     assert part_2(input) is not None
+def test_part_2():
+    input = get_example_input()
+    assert part_2(input) == 26984457539
 
 
-# def test_part_1_real():
-#     input = read_input(__file__)
-#     assert part_1(input) is not None
+def test_part_1_real():
+    input = read_input(__file__)
+    assert part_1(input) == 351188
 
 
-# def test_part_2_real():
-#     input = read_input(__file__)
-#     assert part_2(input) is not None
+def test_part_2_real():
+    input = read_input(__file__)
+    assert part_2(input) == 1595779846729
 
 
 # -- Main
