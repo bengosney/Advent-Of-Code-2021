294b34c6d9ffdde491841343bf3bbcb099956483
Ben Gosney <bengosney@googlemail.com>
2021-12-09 13:45:41 +0000

    More simplification

--- src/day_09.py
+++ src/day_09.py
@@ -21,39 +21,30 @@ def get_grid(input: str) -> Grid:
     return grid
 
 
-def get_adjacent(grid: Grid, position: Position, default: int) -> Grid:
+def get_adjacent(grid: Grid, position: Position) -> Grid:
     x, y = position
     adjacent_positions = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
-    return {position: grid.get(position, default) for position in adjacent_positions}
+    return {adjacent: grid[adjacent] for adjacent in adjacent_positions if adjacent in grid}
+
+
+def get_adjacent_min(grid: Grid, position: Position) -> int:
+    return min(get_adjacent(grid, position).values())
 
 
 def get_lowpoints(grid: Grid) -> Grid:
-    maxheight = max(grid.values())
-    lowpoints = {}
-    for position, height in grid.items():
-        adjacent = get_adjacent(grid, position, maxheight)
-        if height < min(adjacent.values()):
-            lowpoints[position] = grid[position] + 1
-
-    return lowpoints
+    return {position: grid[position] + 1 for position, height in grid.items() if height < get_adjacent_min(grid, position)}
 
 
 def part_1(input: str) -> int:
     grid = get_grid(input)
 
-    lowpoints = get_lowpoints(grid)
-
-    return sum(lowpoints.values())
+    return sum(get_lowpoints(grid).values())
 
 
 def find_basin(grid: Grid, position: Position) -> set[Position]:
-    x, y = position
-
-    adjacent_cells = get_adjacent(grid, (x, y), 0)
-
-    out: set[Position] = {(x, y)}
-    for adjacent_position, adjacent in adjacent_cells.items():
-        if adjacent > grid[(x, y)] and adjacent < 9:
+    out: set[Position] = {position}
+    for adjacent_position, height in get_adjacent(grid, position).items():
+        if height > grid[position] and height < 9:
             out |= find_basin(grid, adjacent_position)
 
     return out
