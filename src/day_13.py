5abb31a2ae4a323766095b365e3d3f135a305ac8
Ben Gosney <bengosney@googlemail.com>
2021-12-14 15:01:21 +0000

    Move the ORC into the utils

--- src/day_13.py
+++ src/day_13.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from typing import Iterator
 
 # First Party
-from utils import read_input
+from utils import ocr, read_input
 
 Position = tuple[int, int]
 Fold = tuple[str, int]
@@ -54,44 +54,6 @@ def fold_grid(grid: Grid, fold: Fold) -> Grid:
     return grid
 
 
-def parse_output(input: Grid) -> str:
-    ALPHABET = {
-        ".##.\n#..#\n#..#\n####\n#..#\n#..#": "A",
-        "###.\n#..#\n###.\n#..#\n#..#\n###.": "B",
-        ".##.\n#..#\n#...\n#...\n#..#\n.##.": "C",
-        "####\n#...\n###.\n#...\n#...\n####": "E",
-        "####\n#...\n###.\n#...\n#...\n#...": "F",
-        ".##.\n#..#\n#...\n#.##\n#..#\n.###": "G",
-        "#..#\n#..#\n####\n#..#\n#..#\n#..#": "H",
-        ".###\n..#.\n..#.\n..#.\n..#.\n.###": "I",
-        "..##\n...#\n...#\n...#\n#..#\n.##.": "J",
-        "#..#\n#.#.\n##..\n#.#.\n#.#.\n#..#": "K",
-        "#...\n#...\n#...\n#...\n#...\n####": "L",
-        ".##.\n#..#\n#..#\n#..#\n#..#\n.##.": "O",
-        "###.\n#..#\n#..#\n###.\n#...\n#...": "P",
-        "###.\n#..#\n#..#\n###.\n#.#.\n#..#": "R",
-        ".###\n#...\n#...\n.##.\n...#\n###.": "S",
-        "#..#\n#..#\n#..#\n#..#\n#..#\n.##.": "U",
-        "#...\n#...\n.#.#\n..#.\n..#.\n..#.": "Y",
-        "####\n...#\n..#.\n.#..\n#...\n####": "Z",
-        "....\n....\n....\n....\n....\n....": " ",
-        "####\n#...\n#...\n#...\n####\n....": "TEST",
-    }
-
-    all_x, _ = zip(*input)
-    chars: str = ""
-    char_count = max(all_x) // 4
-    for i in range(char_count + 1):
-        char: str = ""
-        for y in range(6):
-            for x in range(4):
-                char += input[(x + (i * 5), y)]
-            char += "\n"
-        chars += ALPHABET[char.strip()]
-
-    return "".join(chars).strip()
-
-
 def part_1(input: str) -> int:
     raw_cords, raw_folds = input.split("\n\n")
     cords = parse_coords(raw_cords.split("\n"))
@@ -120,7 +82,7 @@ def part_2(input: str) -> str:
     for fold in folds:
         grid = fold_grid(grid, fold)
 
-    return parse_output(grid)
+    return ocr(grid)
 
 
 # -- Tests
@@ -168,14 +130,6 @@ def test_part_1_real():
 def test_part_2_real():
     input = read_input(__file__)
     assert part_2(input) == "UFRZKAUZ"
-
-
-def test_parse_output():
-    grid: Grid = {}
-    for y, line in enumerate(".##.\n#..#\n#..#\n####\n#..#\n#..#".split("\n")):
-        for x, cell in enumerate(line):
-            grid[(x, y)] = cell
-    assert parse_output(grid) == "A"
 
 
 # -- Main
